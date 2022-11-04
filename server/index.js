a85037c13e074e9c2c56be290c455a8313f4d2a3
Janhavi <patil.janhavi1207@gmail.com>
2022-11-04 20:22:20 +0530

    backend setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+// const User = require('./models/userSchema.js')
+
+//middleware
+app.use(express.json());
+
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+    // console.log(req.path, req.method);
+    next();
+})
+
+
+app.use(require('./routes/auth'))
+
+
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        //listen on port 4000
+        app.listen(8000, () => {
+            console.log(`Connected to Mong0DB and Listening on Port 3000`)
+        })
+    }).catch((error) => {
+        console.log(error)
+    })
+
